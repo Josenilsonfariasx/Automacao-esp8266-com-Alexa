dace415da968a741572399c1d891d36f21197825
Josenilson Farias <101416192+Josenilsonfariasx@users.noreply.github.com>
2022-09-09 08:19:04 -0300

    atualização

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
- #include <Arduino.h>
+#include <Arduino.h>
 #ifdef ESP32
 #include <WiFi.h>
 #define RELAY_PIN_1 12
@@ -57,33 +57,22 @@ void setup() {
   pinMode(RELAY_PIN_2, OUTPUT);
   digitalWrite(RELAY_PIN_2, LOW);
 
-  // Por padrão, fauxmoESP cria seu próprio servidor web na porta definida
-  // A porta TCP deve ser 80 para dispositivos gen3 (o padrão é 1901)
-  // Isso deve ser feito antes da chamada enable()
-  fauxmo.createServer(true); // não é necessário, este é o valor padrão
-  fauxmo.setPort(80); // Isso é necessário para dispositivos gen3
+  
+  fauxmo.createServer(true); 
+  fauxmo.setPort(80); 
 
-  // Você deve chamar enable(true) assim que tiver uma conexão WiFi
-  // Você pode ativar ou desativar a biblioteca a qualquer momento
-  // Desativá-lo impedirá que os dispositivos sejam descobertos e trocados
+  
   fauxmo.enable(true);
 
-  // Você pode usar diferentes maneiras de chamar a Alexa para modificar o estado dos dispositivos:
-  // "Alexa, ligar LED um"
 
-  // Adiciona os dispositivos
   fauxmo.addDevice(LAMP_1);
   fauxmo.addDevice(LAMP_2);
 
   fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
-    // Retorno de chamada quando um comando da Alexa é recebido.
-    // Você pode usar device_id ou device_name para escolher o elemento no qual realizar uma ação (relé, LED, ...)
-    // O state é um booleano (ON / OFF) e value um número de 0 a 255 (se você disser "definir a luz da cozinha para 50%", receberá 128 aqui).
 
     Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
     if ( (strcmp(device_name, LAMP_1) == 0) ) {
       Serial.println("RELAY 1 switched by Alexa");
-      //digitalWrite(RELAY_PIN_1, !digitalRead(RELAY_PIN_1));
       if (state) {
         digitalWrite(RELAY_PIN_1, HIGH);
       } else {
@@ -91,7 +80,6 @@ void setup() {
       }
     }
     if ( (strcmp(device_name, LAMP_2) == 0) ) {
-      // this just sets a variable that the main loop() does something about
       Serial.println("RELAY 2 switched by Alexa");
       if (state) {
         digitalWrite(RELAY_PIN_2, HIGH);
@@ -104,8 +92,7 @@ void setup() {
 }
 
 void loop() {
-  // fauxmoESP usa um servidor TCP assíncrono, mas um servidor UDP sincronizado
-  // Portanto, temos que pesquisar manualmente os pacotes UDP
+
   fauxmo.handle();
 
   static unsigned long last = millis();
